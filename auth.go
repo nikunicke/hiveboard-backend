2afeec46596996c62af24917e54c83b4fd3a9740
Niklas Pimenoff <npimenof@mpro.local>
2020-03-30 22:52:26 +0300

    added http.Client as variable to make get request more seamless

--- auth.go
+++ auth.go
@@ -1,6 +1,7 @@
 package hiveboard
 
 import (
+	"net/http"
 	"os"
 
 	"golang.org/x/oauth2"
@@ -10,4 +11,5 @@ var (
 	OauthConf  *oauth2.Config
 	OauthState = os.Getenv("STATE42")
 	OauthToken *oauth2.Token
+	Client     *http.Client
 )
