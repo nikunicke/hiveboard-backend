e39319d3b1c36df42fca47eae778eecc0ff724ca
Niklas Pimenoff <npimenof@mpro.local>
2020-03-30 22:54:27 +0300

    Split up server setup to its own function

--- cmd/hiveboard/main.go
+++ cmd/hiveboard/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"log"
+
+	"github.com/nikunicke/hiveboard/bolt"
 	"github.com/nikunicke/hiveboard/http"
 )
 
 func main() {
-	http.Run()
+	// http.Run()
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Run() error {
+	httpServer := http.NewServer()
+	httpServer.Addr = ":3000"
+	httpServer.EventService = bolt.NewEventService()
+	err := httpServer.Open()
+	if err != nil {
+		return err
+	}
+	return nil
 }
