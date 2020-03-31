e0949042ed088e3a50b99846098552b7493f5565
Niklas Pimenoff <npimenof@mpro.local>
2020-03-31 23:21:55 +0300

    added some error messages

--- error.go
+++ error.go
@@ -1,7 +1,9 @@
 package hiveboard
 
 const (
-	ErrAuth = Error("Authorization token missing")
+	ErrAuth       = Error("Authorization token missing")
+	UserNotFound  = Error("User does not exist")
+	EventNotFound = Error("Event does not exist")
 )
 
 type Error string
