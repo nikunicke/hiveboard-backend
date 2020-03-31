bc9f7f0a69c8741e9422b9c213024337386b806f
Niklas Pimenoff <npimenof@mpro.local>
2020-03-31 14:35:31 +0300

    added user handler, part of the previous commit

---
+++ bolt/user.go
@@ -0,0 +1,34 @@
+package bolt
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/nikunicke/hiveboard"
+)
+
+type UserService struct {
+	user *hiveboard.User
+}
+
+func NewUserService() *UserService {
+	return &UserService{
+		user: nil,
+	}
+}
+
+func (s *UserService) GetUser(url string) (*hiveboard.User, error) {
+	var user *hiveboard.User
+	response, err := hiveboard.Client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(body, &user); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
